37eb7c39e8079c24da9d4fa719acab27ef387e38
unknown <soniahnjogu435@gmail.com>
2023-03-06 05:21:48 -0800

    intro to Oopp

---
+++ oop/House_A.py
@@ -0,0 +1,7 @@
+class House:
+    def __init__(self, gate, door, rooms, windows):
+        self.gate = gate
+        self.door = door
+        self.rooms = rooms
+        self.windows = windows
+
---
+++ oop/House_p.py
@@ -0,0 +1,9 @@
+from House_A import House
+
+
+House_1 = House("Steel", "wooden", "5", "12")
+House_2 = House("Vinyl", "glass", "6", "14")
+House_3 = House("Aluminium", "Vinyl", "10", "18")
+print(House_1.gate, House_1.door, House_1.rooms, House_1.windows)
+print(House_2.gate, House_2.door, House_2.rooms, House_2.windows)
+print(House_3.gate, House_3.door, House_3.rooms, House_3.windows)
