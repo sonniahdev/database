6b1d89f1b33524ac981ac093dd6b23df67e4060d
unknown <soniahnjogu435@gmail.com>
2023-03-07 04:23:02 -0800

    into to Oop_2

---
+++ oop/Animal.py
@@ -0,0 +1,6 @@
+class Animal:
+    def __init__(self, gender, legs, teeth, color):
+        self.gender = gender
+        self.legs = legs
+        self.teeth = teeth
+        self.color = color
---
+++ oop/Animal_2.py
@@ -0,0 +1,7 @@
+from Animal import Animal
+Animal_1 = Animal("male", "2", "32", "dark")
+Animal_2 = Animal("female", "4", "24", "grey")
+Animal_3 = Animal("male", "8", "10", "brown")
+print(Animal_1.gender, Animal_1.legs, Animal_1.teeth, Animal_1.color)
+print(Animal_2.gender, Animal_2.legs, Animal_2.teeth, Animal_2.color)
+print(Animal_3.gender, Animal_3.legs, Animal_3.teeth, Animal_3.color)
